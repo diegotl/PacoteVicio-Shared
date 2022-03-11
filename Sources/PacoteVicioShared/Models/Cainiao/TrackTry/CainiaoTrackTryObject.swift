19b1a146a2030dff5a88262eff621fa886142747
Diego Trevisan Lara <diego@trevisa.nl>
2022-03-12 00:07:18 +0100

    Remove unnecessary properties from CainiaoTrackTryObject

--- Sources/PacoteVicioShared/Models/Cainiao/TrackTry/CainiaoTrackTryObject.swift
+++ Sources/PacoteVicioShared/Models/Cainiao/TrackTry/CainiaoTrackTryObject.swift
@@ -5,16 +5,12 @@ public final class CainiaoTrackTryObject<E: CainiaoEvent>: CainiaoObject {
     // MARK: - Private variables
     let lastEvent: String
     let lastUpdateTime: String
-    let status: String
-    let trackUpdate: Bool
     let trackinfo: [E]?
     let weblink: String
 
     enum CodingKeys: String, CodingKey {
         case lastEvent
         case lastUpdateTime
-        case status
-        case trackUpdate = "track_update"
         case trackinfo
         case trackingNumber = "tracking_number"
         case weblink
